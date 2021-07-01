28a19f81a8132340a9fc2471fb6e9308854cb9be
Robert Ruff <robruff@gmail.com>
2021-06-30 23:35:54 -0400

    Added a progress bar.

--- downloader.py
+++ downloader.py
@@ -1,30 +1,53 @@
 #!/usr/bin/env python3
 
+import pathlib
+import progressbar
 import requests
 import sys
 
+"""
+A simple HTTP download client.
+
+@params:
+    download_dir    - Optional  :   base directory for downloads--if not provided a full path is expected for filenames
+
+"""
 class Downloader:
-    def __init__(self, requests):
-        self.requests = requests
+    def __init__(self, download_dir=None):
+        try:
+            self.download_dir = pathlib.PurePath(download_dir)
+        except TypeError:
+            self.download_dir = download_dir
 
     def get(self, url, filename):
         resp = self._get_file(url)
         self._save(filename, resp)
     
     def _get_file(self, url):
-        response = self.requests.get(url)
+        response = requests.get(url, stream=True)
         response.raise_for_status()
         return response
 
-    def _save(self, fname, request_data):
+    def _save(self, fname, r):
+        if self.download_dir:
+            fname = self.download_dir.joinpath(fname)
+        
+        pbar = self._init_pbar(r)
         with open(fname, 'wb') as f:
-            for chunk in request_data.iter_content(100000):
+            progress = 0
+            for chunk in r.iter_content(1024):
+                progress += len(chunk)
+                pbar.update(progress)
                 f.write(chunk)
-
+    
+    def _init_pbar(self, response):
+        total = int(response.headers.get('content-length', 0))
+        return progressbar.ProgressBar(total)
+    
 if __name__ == '__main__':
     url = sys.argv[1]
-    filename = sys.argv[2]
+    filename = pathlib.PurePath(sys.argv[2])
 
-    downloader = Downloader(requests)
+    downloader = Downloader(download_dir=filename.parent.as_posix())
     print(f"Downloading {url} to {filename}")
-    downloader.get(url, filename)
+    downloader.get(url, filename.name)
---
+++ progressbar.py
@@ -0,0 +1,32 @@
+import time
+
+class ProgressBar:
+    def __init__(self, total_size, decimal_places = 1, fill = '█', bar_len = 100):
+        self.total_size = total_size
+        self.places = decimal_places
+        self.fill = fill
+        self.bar_len = bar_len
+
+    def update(self, progress):
+        percent = self._percent_complete(progress)
+        bar = self._bar(progress)
+        print(f'\r| {bar} | {percent}%', end='\r')
+
+        if progress == self.total_size:
+            print()
+
+    def _percent_complete(self, progress):
+        """ Returns the percent complete as a formatted string. """
+        return ("{0:." + str(self.places) + "f}").format(100 * (progress / float(self.total_size)))
+
+    def _bar(self, progress):
+        filled_len = int(self.bar_len * progress // self.total_size)
+        return self.fill * filled_len + '-' * (self.bar_len - filled_len)
+    
+if __name__ == '__main__':
+    progbar = ProgressBar(500)
+    for i in range(0, 501):
+        if i % 10 == 0:
+            time.sleep(0.1)
+            # print(f'{i} / 500 = {i/500}')
+            progbar.update(i)
